8457955ac3a85d0ad6d20ad68ac4ee4758979483
Mauricio Matamoros <mauricio.enrique.matamoros@gmail.com>
2015-12-05 15:40:56 -0600

    clases

---
+++ Salmans/src/salmans/Products.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salmans;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Products {
+    private String name;
+    private int time;
+    private int price;
+    private ArrayList<Ingredients> ingredients = new ArrayList();
+
+    public Products(String name, int time, int price) {
+        this.name = name;
+        this.time = time;
+        this.price = price;
+    }
+
+    public Products() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public ArrayList<Ingredients> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredients> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Override
+    public String toString() {
+        return "Products{" + "name=" + name + ", time=" + time + ", price=" + price + ", ingredients=" + ingredients + '}';
+    }
+    
+}
---
+++ Salmans/src/salmans/Trucks.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package salmans;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mauricio
+ */
+public class Trucks {
+    private int ID;
+    private ArrayList<Products> products = new ArrayList();
+
+    public Trucks() {
+    }
+
+    public Trucks(int ID) {
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public ArrayList<Products> getProducts() {
+        return products;
+    }
+
+    public void setProducts(ArrayList<Products> products) {
+        this.products = products;
+    }
+
+    @Override
+    public String toString() {
+        return "Trucks{" + "ID=" + ID + ", products=" + products + '}';
+    }
+    
+}
